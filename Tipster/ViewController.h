d7f74c4f243aa72fd9e79e9c434bf9107e9efa06
Giovanna Lemos Ribeiro <gilemos@fb.com>
2019-06-25 13:49:58 -0700

    Initial Commit

---
+++ Tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipster
+//
+//  Created by gilemos on 6/25/19.
+//  Copyright © 2019 gilemos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
